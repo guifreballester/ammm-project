29a2a2e10dbb63e7f0d4485e3869cb637f6ddb05
Sergi <sergi@gmail.com>
2017-12-27 16:58:27 +0100

    Updated data values

--- GRASP/data.py
+++ GRASP/data.py
@@ -1,21 +1,35 @@
-# data = {
-#     'numNurses': 2000,
-#     'hours': 24,
-#     'demand': [8, 8, 4, 4, 4, 8, 8, 12, 16, 24, 24, 28, 20, 32,
-#                32, 28, 24, 24, 16, 12, 16, 12, 12, 12],
-#     'minHours': 5,
-#     'maxHours': 9,
-#     'maxConsec': 3,
-#     'maxPresence': 14,
-# }
+data_1 = {
+    'numNurses': 2000,
+    'hours': 24,
+    'demand': [8, 8, 4, 4, 4, 8, 8, 12, 16, 24, 24, 28, 20, 32,
+               32, 28, 24, 24, 16, 12, 16, 12, 12, 12],
+    'minHours': 5,
+    'maxHours': 9,
+    'maxConsec': 3,
+    'maxPresence': 14,
+}
 
-data = {
+data_2 = {
     'numNurses': 10,
     'hours': 10,
     'demand': [2, 2, 1, 1, 1, 2, 2, 3, 4, 6],
-    'minHours': 5,
+    'minHours': 2,
     'maxHours': 9,
     'maxConsec': 3,
     'maxPresence': 10,
 }
+
+data_3 = {
+    'numNurses': 20,
+    'hours': 24,
+    'demand': [2, 2, 1, 1, 1, 2, 2, 3, 4, 6, 6,
+                7, 5, 8, 8, 7, 6, 6, 4, 3, 4, 3, 3, 3],
+    'minHours': 5,
+    'maxHours': 9,
+    'maxConsec': 3,
+    'maxPresence': 14,
+}
+
+
+
 
